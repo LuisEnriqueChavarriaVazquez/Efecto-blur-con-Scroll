323405e9ecbca4c4e4327da0dfe4acee6a3f9a42
Luis Enrique <luisechvz@gmail.com>
2019-02-04 21:33:59 -0600

    Agregamos el archivo README.md y tambien algunas imagenes

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pagina web de Scroll con BLUR.
+
+Esta página web es un pequeño ejemplo del efecto blur en combinación de scroll en la pantalla con el uso de JQUERY.
+
+#### Normal
+
+![](img/1.png)
+
+#### Un poco de scroll
+
+![](img/2.png)
+
+#### Mucho scroll
+
+![](img/3.png)
--- js/main.js
+++ js/main.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
     }
 
     ajustesIniciales();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        var pixels = scrollTop / 70;
+
+        if (scrollTop < height) {
+            $(".contenedorGeneral").css({
+                "-webkit-filter": "blur(" + pixels + "px)",
+                "background-position": "center -" + pixels * 10 + "px"
+            });
+        }
+
+    });
+
 });
